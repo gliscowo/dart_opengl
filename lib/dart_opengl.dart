9fad60e42c6a162c297d2ecf76a9c56ef3a815f0
glisco <glisco03@web.de>
2023-09-29 21:55:56 +0200

    init moment

---
+++ lib/dart_opengl.dart
@@ -0,0 +1,8 @@
+/// Dart bindings for the Open Graphics Library
+///
+/// Includes core + compatibility profile functionality of all
+/// versions including default extensions
+library;
+
+export 'src/gl_constants.dart';
+export 'src/gl_init.dart' show loadOpenGL;
